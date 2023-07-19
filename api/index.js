f4c1a86fc24e4407705b64e4a4b82589e18cbd57
Noelia <noelia.martino@live.com.ar>
2023-07-19 13:22:58 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const saveApiData = require("./src/controllers/getApiData.js");
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
 // Syncing all the models at once.
 //Cambiar a false para empezar a guardar los post
